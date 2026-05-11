99181973f8403f6bc35f3ddff2c09c28a8bbc5bc
omar-hossam <omarhossam50@proton.me>
2026-05-11 22:45:20 +0300

    feat: implement GET weather method

--- src/weather.js
+++ src/weather.js
@@ -1,3 +1,5 @@
+import { getDateStr } from './helpers.js'
+
 export async function searchCity(cityName) {
   const response = await fetch(
     `https://geocoding-api.open-meteo.com/v1/search?name=${cityName}`
@@ -17,6 +19,26 @@ export async function searchCity(cityName) {
 }
 
 
+export async function getWeather(lat, lon, day) {
+  const response = await fetch(
+  `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=temperature_2m_max,temperature_2m_min,wind_speed_10m_max,precipitation_sum,weather_code&start_date=${day}&end_date=${day}&timezone=auto`);
+  
+  if (!response.ok) {
+    throw new Error("Failed to fetch weather");
+  }
+  
+  const data = await response.json();
+  
+  console.log(data.results)
+  
+  if (!data.daily || data.daily.length === 0) {
+    throw new Error("Weather API not working");
+  }
+  
+  return data.daily;
+}
+
+
 export async function scan(e) {
   e.preventDefault();
   
@@ -27,10 +49,18 @@ export async function scan(e) {
     this.lon = searchResult.longitude;
 
     this.errMsg = '';
-    console.log(this.lat)
-    console.log(this.lon)
     
-  } catch (e) {
-    this.errMsg = e.message;
+    try {
+      const day = getDateStr(this.day) // Format: YYYY-MM-DD
+      
+      const weatherResult = await getWeather(this.lat, this.lon, day);
+      console.log(weatherResult)
+      
+    } catch (e) {
+      this.errMsg = e.message;
+    }
+    
+  } catch (error) {
+    this.errMsg = error.message;
   }
 }
